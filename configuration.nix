b8b8eeec659f55602c6033199fd96b7b5865694d
wesleytruong <wesleytruong@ucsb.edu>
2024-12-16 18:55:25 -0800

    removed nixos dir

--- configuration.nix
+++ configuration.nix
@@ -97,6 +97,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    libgcc
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
